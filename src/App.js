183494df2d1ad679f3a10259eb307d63d9e0c2d8
Mayank-Sharma-27 <mayank@Mayanks-MacBook-Air.local>
2023-08-27 00:21:48 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Experience from './pages/Experience';
+import Blogs from './pages/Blogs';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/experience' element={<Experience />}/>
+          <Route path='/blogs' element={<Blogs />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
