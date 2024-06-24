b718ae800b7182ccf441c7874e4eea4f7d213e52
Mayank-Sharma-27 <mayank.sharma2796@gmail.com>
2024-06-23 23:43:44 -0700

    Update resume and information

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,12 +4,18 @@ export const  Modal =  ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
     return (
-        <div className={styles.overlay}>
-            <div className={styles.content}>
-                <button className={styles.closeButton} onClick={onClose}>X</button>
-                <iframe className={styles.resumeFrame} src="https://drive.google.com/file/d/15UsfENyoaI0TNxi0iftmu9eS182cZ7GK/preview" title="Resume" />
-            </div>
+      <div className={styles.overlay}>
+        <div className={styles.content}>
+          <button className={styles.closeButton} onClick={onClose}>
+            X
+          </button>
+          <iframe
+            className={styles.resumeFrame}
+            src="https://drive.google.com/file/d/1ltDuE3-Oqubn-EcDal7zkiWHncK8pZbS/view?usp=sharing"
+            title="Resume"
+          />
         </div>
+      </div>
     );
 }
 
